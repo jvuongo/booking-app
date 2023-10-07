691fbe199877ad1838d7ea5eb62bd93eef6e19e8
jvuongo <jack.vuong12@gmail.com>
2023-10-07 14:36:02 +1100

    Learned concurrency and structs

--- main.go
+++ main.go
@@ -2,56 +2,62 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sync"
+	"time"
 )
 
 var conferenceName = "Go Conference" // This syntax only works for variables without explicit type declaration
 const conferenceTickets int = 50
 
 var remainingTickets uint = 50 // uint means that it's a positive int and never goes negative
-var bookings = []string{}
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
 
 func main() {
 
 	greetUser()
 
-	for remainingTickets > 0 && len(bookings) < 50 {
+	// func call for user input
+	firstName, lastName, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
-		// func call for user input
-		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+	if isValidName && isValidEmail && isValidTicketNumber {
 
-		if isValidName && isValidEmail && isValidTicketNumber {
+		//call func for booking
+		bookTicket(userTickets, firstName, lastName, email)
+		wg.Add(1)
+		go sendTicket(userTickets, firstName, lastName, email) // "go" abstracts the thread creation
+		// call function print first names
+		fmt.Printf("These are all the bookings: %v\n", getFirstNames())
 
-			//call func for booking
-			bookings = bookTicket(userTickets, firstName, lastName, email)
-			// call function print first names
-			fmt.Printf("These are all the bookings: %v\n", getFirstNames())
+		var noTicketsRemaining bool = remainingTickets == 0
 
-			var noTicketsRemaining bool = remainingTickets == 0
-
-			if noTicketsRemaining {
-				// end program
-				fmt.Println("Our conference is booked out. Come back next year.")
-				break
-			}
-		} else {
-			if !isValidName {
-				fmt.Println("First name or last name is too short.")
-			}
-
-			if !isValidEmail {
-				fmt.Println("Email address is not valid.")
-			}
-
-			if !isValidTicketNumber {
-				fmt.Println("Number of tickets is invalid")
-			}
-			continue
+		if noTicketsRemaining {
+			// end program
+			fmt.Println("Our conference is booked out. Come back next year.")
+		}
+	} else {
+		if !isValidName {
+			fmt.Println("First name or last name is too short.")
 		}
 
-	}
+		if !isValidEmail {
+			fmt.Println("Email address is not valid.")
+		}
 
+		if !isValidTicketNumber {
+			fmt.Println("Number of tickets is invalid")
+		}
+	}
+	wg.Wait()
 }
 
 func greetUser() {
@@ -63,19 +69,9 @@ func greetUser() {
 func getFirstNames() []string {
 	var firstNames = []string{}
 	for _, bookings := range bookings { // use _ because we are telling Go that we are not going be using the index variable
-		var names = strings.Fields(bookings)
-		var firstName = names[0]
-		firstNames = append(firstNames, firstName)
+		firstNames = append(firstNames, bookings.firstName)
 	}
 	return firstNames
-}
-
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
-	var isValidEmail bool = strings.Contains(email, "@")
-	var isValidTicketNumber bool = userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidName, isValidEmail, isValidTicketNumber
 }
 
 func getUserInput() (string, string, string, uint) {
@@ -100,12 +96,30 @@ func getUserInput() (string, string, string, uint) {
 	return firstName, lastName, email, userTickets
 }
 
-func bookTicket(userTickets uint, firstName string, lastName string, email string) []string {
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName) // slices instead of array, slices are an abstraction of an array
+
+	// create a map for a user
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
+
+	bookings = append(bookings, userData) // slices instead of array, slices are an abstraction of an array
+	fmt.Printf("List of bookings: %v\n", bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
 
-	return bookings
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second) // stopping this thread for 10 seconds
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
+	fmt.Println("################################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
+	fmt.Println("################################")
+
+	wg.Done()
 }
